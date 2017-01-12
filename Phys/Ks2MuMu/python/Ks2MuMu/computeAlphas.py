f764a4d51afe4a0feae73e00e92c165654b004ce
Miguel Ramos Pernas <miguel.ramos.pernas@cern.ch>
2017-01-12 17:01:37 +0100

    Updated computeAlphas.py

--- Phys/Ks2MuMu/python/Ks2MuMu/computeAlphas.py
+++ Phys/Ks2MuMu/python/Ks2MuMu/computeAlphas.py
@@ -1,14 +1,14 @@
 from ROOT import *
 
 # Global factors
-br_kspipi    = 0.69
-lumi_gain    = 2.
+br_kspipi = 0.69
+lumi_gain = 2.
 
 # Get alpha_2011
 import fiducial_2011
 smb_2011              = 2.7e-6
 trig_presc_2011       = 0.5
-gen_eff_cut_mumu_2011 = 0.5483
+gen_eff_cut_mumu_2011 = 0.27
 
 path_2011      = '/afs/cern.ch/user/d/diegoms/vol5/ks/'
 fkspipi_2011   = TFile(path_2011 + 'KsPiPi_MC11_ntuples.root')
@@ -42,18 +42,18 @@ sel_eff_2011 = gen_eff_cut_mumu_2011*ksmumu_2011.GetEntries(mumuselcuts_2011)*1.
 # Get alpha_2012
 import fiducial
 
-gen_eff_cut_mumu_2012 = 0.26510
+gen_eff_cut_mumu_2012 = 0.27
 smb_2012              = 3.39e-7
 
 path_2012       = '/eos/lhcb/wg/RD/K0SMuMu/NEW/'
-fkspipimb_2012  = TFile(path_2012 + 'DATA/KsMB_Data2012_MVA.root')
-fkspipi_2012    = TFile(path_2012 + 'MC/Kspipi_MC2012_mcMatch_MVA.root')
-fkspipimc_2012  = TFile(path_2012 + 'MC/Kspipi_MC2012_mcMatch.root')
-fksmumu_2012    = TFile(path_2012 + 'MC/Ksmumu_MC2012_mcMatch_MVA.root')
-fksmumumc_2012  = TFile(path_2012 + 'MC/Ksmumu_MC2012_mcMatch.root')
-fksmumustr_2012 = TFile(path_2012 + 'MC/Ksmumu_StrippedMC2012_mcMatch_MVA.root')
-kspipi_2012     = fkspipi_2012.Get('DecayTree')
-kspipimc_2012   = fkspipimc_2012.Get('MCtruthKspipi/MCDecayTree')
+#fkspipimb_2012  = TFile(path_2012 + 'DATA/KsMB_Data2012_MVA.root')
+#fkspipi_2012    = TFile(path_2012 + 'MC/Kspipi_MC2012_mcMatch_MVA.root')
+#fkspipimc_2012  = TFile(path_2012 + 'MC/Kspipi_MC2012_mcMatch.root')
+#fksmumu_2012    = TFile(path_2012 + 'MC/Ksmumu_MC2012_mcMatch_MVA.root')
+#fksmumumc_2012  = TFile(path_2012 + 'MC/Ksmumu_MC2012_mcMatch.root')
+#fksmumustr_2012 = TFile(path_2012 + 'MC/Ksmumu_StrippedMC2012_mcMatch_MVA.root')
+#kspipi_2012     = fkspipi_2012.Get('DecayTree')
+#kspipimc_2012   = fkspipimc_2012.Get('MCtruthKspipi/MCDecayTree')
 ksmumu_2012     = fksmumu_2012.Get('DecayTree')
 ksmumumc_2012   = fksmumumc_2012.Get('MCtruthKsmumu/MCDecayTree')
 kspipimb_2012   = fkspipimb_2012.Get('DecayTree')
@@ -63,7 +63,7 @@ sel_eff_2012 = gen_eff_cut_mumu_2012*ksmumu_2012.GetEntries('StrippingKS02MuMuLi
                                                                 'Decision && ' +
                                                             fiducial.fullsel)*1./ksmumumc_2012.GetEntries()
 
-eff_trigger_2012 = ksmumustr_2012.GetEntries(fiducial.SEL['TOS1_'])*1./ksmumustr_2012.GetEntries()
+eff_trigger_2012 = ksmumustr_2012.GetEntries(fiducial.SEL['TOS1_']  + " && StrippingKS02MuMuLineDecision")*1./ksmumustr_2012.GetEntries(fiducial.fullsel + " && StrippingKS02MuMuLineDecision")
 
 trigger_gain = eff_trigger_2012/eff_trigger_2011
 
@@ -71,4 +71,6 @@ alpha_2012 = alpha_2011*sel_eff_2011/(trigger_gain*lumi_gain*sel_eff_2012)
 
 print '--- Alpha 2012 extrapolated:', alpha_2012
 print '--- Expected:', 7e-11
+print '--- Trigger efficiency 2011:', eff_trigger_2011
+print '--- Trigger efficiency 2012:', eff_trigger_2012
 print '--- Trigger gain:', trigger_gain
