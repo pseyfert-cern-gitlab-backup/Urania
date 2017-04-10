11429f79959f1b0f56580e43a8e44dc92868b0cb
Alex Birnkraut <a.birnkraut@cern.ch>
2017-04-10 17:48:17 +0200

    reverted change in WS.py introduced end of last year in the CKM rush

--- PhysFit/B2DXFitters/python/B2DXFitters/WS.py
+++ PhysFit/B2DXFitters/python/B2DXFitters/WS.py
@@ -10,7 +10,7 @@
 from ROOT import RooFit
 
 def WS(ws, obj, opts = [RooFit.RecycleConflictNodes(), RooFit.Silence()]):
-#def WS(ws, obj, opts = []):
+# def WS(ws, obj, opts = []):
     """ "swallow" object into a workspace, returns swallowed object """
     name = obj.GetName()
     wsobj = ws.obj(name)
@@ -30,7 +30,7 @@ def WS(ws, obj, opts = [RooFit.RecycleConflictNodes(), RooFit.Silence()]):
             wsobj = ws.set(name)
         else:
             if wsobj.Class() != obj.Class():
-                raise TypeError()    
+                raise TypeError()
     else:
         if None == wsobj:
             ws.__getattribute__('import')(obj, name)
