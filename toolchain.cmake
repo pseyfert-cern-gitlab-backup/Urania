365df8bafe8fd6e8f9f36773abf9864efae78219
Carlos Vázquez Sierra <carlos.vazquez@cern.ch>
2017-11-08 13:04:29 +0100

    Minor modification probably useless about toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,7 +1,8 @@
 # Special wrapper to load the declared version of the heptools toolchain.
 set(heptools_version 91)
 
-if(DEFINED ENV{HEPTOOLS_VERSION})
+cmake_minimum_required(VERSION 2.8.5)
+if($ENV{HEPTOOLS_VERSION})
   set(heptools_version $ENV{HEPTOOLS_VERSION})
 endif()
 
@@ -9,7 +10,7 @@ endif()
 # compiler (without the proper cache)
 if(NOT CMAKE_SOURCE_DIR MATCHES "CMakeTmp")
 
- # Note: normally we should look for GaudiDefaultToolchain.cmake, but in Urania
+ # Note: normally we should look for GaudiDefaultToolchain.cmake, but in Gaudi
  # it is not needed
  include(${CMAKE_SOURCE_DIR}/cmake/GaudiDefaultToolchain.cmake)
 
