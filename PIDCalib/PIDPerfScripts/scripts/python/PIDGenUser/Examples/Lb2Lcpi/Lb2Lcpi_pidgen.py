42c1cac35defc1c381318bd9f7bbad403c523bcc
Konstantin Gizdov <k.gizdov@cern.ch>
2017-11-21 18:28:28 +0100

    clean whitespace

--- PIDCalib/PIDPerfScripts/scripts/python/PIDGenUser/Examples/Lb2Lcpi/Lb2Lcpi_pidgen.py
+++ PIDCalib/PIDPerfScripts/scripts/python/PIDGenUser/Examples/Lb2Lcpi/Lb2Lcpi_pidgen.py
@@ -1,9 +1,9 @@
 import os
 
-# List of input ROOT files with MC ntuples. Format: 
+# List of input ROOT files with MC ntuples. Format:
 #   (inputfile, outputfile, dataset)
 files = [
-  ("root://eoslhcb.cern.ch//eos/lhcb/wg/PID/PIDGen/Validation/Lb2Lcpi/sim08_2012_md.root", "sim08_2012_md_pidgen.root", "MagDown_2012"), 
+  ("root://eoslhcb.cern.ch//eos/lhcb/wg/PID/PIDGen/Validation/Lb2Lcpi/sim08_2012_md.root", "sim08_2012_md_pidgen.root", "MagDown_2012"),
 ]
 
 # Name of the input tree
@@ -21,63 +21,63 @@ seed = None   # No initial seed
 ##seed = 1    # Alternatively, could set initial random seed
 
 # Dictionary of tracks with their PID variables, in the form {branch name}:{pidvars}
-# For each track branch name, {pidvars} is a dictionary in the form {ntuple variable}:{pid config}, 
-#   where 
-#     {ntuple variable} is the name of the corresponding ntuple PID variable without branch name, 
-#   and 
-#     {pid_config} is the string describing the PID configuration. 
+# For each track branch name, {pidvars} is a dictionary in the form {ntuple variable}:{pid config},
+#   where
+#     {ntuple variable} is the name of the corresponding ntuple PID variable without branch name,
+#   and
+#     {pid_config} is the string describing the PID configuration.
 # Run PIDCorr.py without arguments to get the full list of PID configs
 tracks = {
   'h'   : {
-            "V2ProbNNK"  : "pi_V2ProbNNK", 
-            "V2ProbNNpi" : "pi_V2ProbNNpi", 
-            "V2ProbNNp"  : "pi_V2ProbNNp", 
-            "V3ProbNNK"  : "pi_V3ProbNNK", 
-            "V3ProbNNpi" : "pi_V3ProbNNpi", 
-            "V3ProbNNp"  : "pi_V3ProbNNp", 
-            "pidk"       : "pi_CombDLLK", 
-            "pidp"       : "pi_CombDLLp", 
-           }, 
-  'lpi' : { 
-            "V2ProbNNK"  : "pi_V2ProbNNK", 
-            "V2ProbNNpi" : "pi_V2ProbNNpi", 
-            "V2ProbNNp"  : "pi_V2ProbNNp", 
-            "V3ProbNNK"  : "pi_V3ProbNNK", 
-            "V3ProbNNpi" : "pi_V3ProbNNpi", 
-            "V3ProbNNp"  : "pi_V3ProbNNp", 
-            "pidk"       : "pi_CombDLLK", 
-            "pidp"       : "pi_CombDLLp", 
-           }, 
-  'lk'  : { 
-            "V2ProbNNK"  : "K_V2ProbNNK", 
-            "V2ProbNNpi" : "K_V2ProbNNpi", 
-            "V2ProbNNp"  : "K_V2ProbNNp", 
-            "V3ProbNNK"  : "K_V3ProbNNK", 
-            "V3ProbNNpi" : "K_V3ProbNNpi", 
-            "V3ProbNNp"  : "K_V3ProbNNp", 
-            "pidk"       : "K_CombDLLK", 
-            "pidp"       : "K_CombDLLp", 
-           }, 
+            "V2ProbNNK"  : "pi_V2ProbNNK",
+            "V2ProbNNpi" : "pi_V2ProbNNpi",
+            "V2ProbNNp"  : "pi_V2ProbNNp",
+            "V3ProbNNK"  : "pi_V3ProbNNK",
+            "V3ProbNNpi" : "pi_V3ProbNNpi",
+            "V3ProbNNp"  : "pi_V3ProbNNp",
+            "pidk"       : "pi_CombDLLK",
+            "pidp"       : "pi_CombDLLp",
+           },
+  'lpi' : {
+            "V2ProbNNK"  : "pi_V2ProbNNK",
+            "V2ProbNNpi" : "pi_V2ProbNNpi",
+            "V2ProbNNp"  : "pi_V2ProbNNp",
+            "V3ProbNNK"  : "pi_V3ProbNNK",
+            "V3ProbNNpi" : "pi_V3ProbNNpi",
+            "V3ProbNNp"  : "pi_V3ProbNNp",
+            "pidk"       : "pi_CombDLLK",
+            "pidp"       : "pi_CombDLLp",
+           },
+  'lk'  : {
+            "V2ProbNNK"  : "K_V2ProbNNK",
+            "V2ProbNNpi" : "K_V2ProbNNpi",
+            "V2ProbNNp"  : "K_V2ProbNNp",
+            "V3ProbNNK"  : "K_V3ProbNNK",
+            "V3ProbNNpi" : "K_V3ProbNNpi",
+            "V3ProbNNp"  : "K_V3ProbNNp",
+            "pidk"       : "K_CombDLLK",
+            "pidp"       : "K_CombDLLp",
+           },
   'lp'  : {
-            "V2ProbNNK"  : "p_V2ProbNNK", 
-            "V2ProbNNpi" : "p_V2ProbNNpi", 
-            "V2ProbNNp"  : "p_V2ProbNNp", 
-            "V3ProbNNK"  : "p_V3ProbNNK", 
-            "V3ProbNNpi" : "p_V3ProbNNpi", 
-            "V3ProbNNp"  : "p_V3ProbNNp", 
-            "pidk"       : "p_CombDLLK", 
-            "pidp"       : "p_CombDLLp", 
-           }, 
+            "V2ProbNNK"  : "p_V2ProbNNK",
+            "V2ProbNNpi" : "p_V2ProbNNpi",
+            "V2ProbNNp"  : "p_V2ProbNNp",
+            "V3ProbNNK"  : "p_V3ProbNNK",
+            "V3ProbNNpi" : "p_V3ProbNNpi",
+            "V3ProbNNp"  : "p_V3ProbNNp",
+            "pidk"       : "p_CombDLLK",
+            "pidp"       : "p_CombDLLp",
+           },
 }
 
 output_tree = input_tree.split("/")[-1]
 treename = input_tree
 
-for input_file, output_file, dataset in files : 
+for input_file, output_file, dataset in files :
   tmpinfile = input_file
   tmpoutfile = "tmp1.root"
-  for track, subst in tracks.iteritems() : 
-    for var, config in subst.iteritems() : 
+  for track, subst in tracks.iteritems() :
+    for var, config in subst.iteritems() :
       command = "python $PIDPERFSCRIPTSROOT/scripts/python/PIDGenUser/PIDGen.py"
 #      command = "python ../../PIDGen.py"
       command += " -m %s_%s" % (track, ptvar)
@@ -90,14 +90,14 @@ for input_file, output_file, dataset in files :
       command += " -d %s" % dataset
       command += " -i %s" % tmpinfile
       command += " -o %s" % tmpoutfile
-      if seed : 
+      if seed :
         command += " -s %d" % seed
 
       treename = output_tree
       tmpinfile = tmpoutfile
-      if tmpoutfile == "tmp1.root" : 
+      if tmpoutfile == "tmp1.root" :
         tmpoutfile = "tmp2.root"
-      else : 
+      else :
         tmpoutfile = "tmp1.root"
 
       print command
