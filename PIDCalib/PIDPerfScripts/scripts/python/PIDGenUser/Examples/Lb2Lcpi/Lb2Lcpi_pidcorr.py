5be1274f991f89ae28cb224bca32beb03265b93c
Konstantin Gizdov <k.gizdov@cern.ch>
2017-11-21 18:30:04 +0100

    make example scripts more general and user friendly

--- PIDCalib/PIDPerfScripts/scripts/python/PIDGenUser/Examples/Lb2Lcpi/Lb2Lcpi_pidcorr.py
+++ PIDCalib/PIDPerfScripts/scripts/python/PIDGenUser/Examples/Lb2Lcpi/Lb2Lcpi_pidcorr.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+## START OF CONFIG
+# Read comments and check vars
+# at least until end of config section
 
 # List of input ROOT files with MC ntuples. Format:
 #   (inputfile, outputfile, dataset)
@@ -16,7 +21,11 @@ simversion = "sim08"
 # e.g. if the ntuple contains "pion_PT", it should be just "PT"
 ptvar = "pt"
 etavar = "eta"
-##pvar = "p"      # Could use P variable instead of eta
+pvar   = None
+## Could use P variable instead of eta
+# etavar = None
+# pvar   = "p"
+
 ntrvar = "nTracks"  # This should correspond to the number of "Best tracks", not "Long tracks"!
 
 # Dictionary of tracks with their PID variables, in the form {branch name}:{pidvars}
@@ -69,19 +78,39 @@ tracks = {
            },
 }
 
+# IF ON LXPLUS: if /tmp exists and is accessible, use for faster processing
+# IF NOT: use /tmp if you have enough RAM
+# temp_folder = '/tmp'
+# ELSE: use current folder
+temp_folder = '.'
+
+## END OF CONFIG
+
+
+# make sure we don't overwrite local files and prefix them with random strings
+import string
+import random
+rand_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))  # get 10 random chars for temp_file prefix
+
+temp_file_prefix = temp_folder + '/' + rand_string  # prefix temp files with folder and unique ID
+
 output_tree = input_tree.split("/")[-1]
 treename = input_tree
 
 for input_file, output_file, dataset in files :
   tmpinfile = input_file
-  tmpoutfile = "tmp1.root"
+  tmpoutfile = "%s_tmp1.root" % temp_file_prefix
   for track, subst in tracks.iteritems() :
     for var, config in subst.iteritems() :
       command = "python $PIDPERFSCRIPTSROOT/scripts/python/PIDGenUser/PIDCorr.py"
-#      command = "python ../../PIDCorr.py"
       command += " -m %s_%s" % (track, ptvar)
+      if etavar:
       command += " -e %s_%s" % (track, etavar)
-##      command += " -q %s_%s" % (track, pvar)   # Could also use P variable instead of eta
+      elif pvar:
+        command += " -q %s_%s" % (track, pvar)
+      else:
+        print('Specify either ETA or P branch name per track')
+        sys.exit(1)
       command += " -n %s" % ntrvar
       command += " -t %s" % treename
       command += " -p %s_%s_corr" % (track, var)
@@ -94,13 +123,17 @@ for input_file, output_file, dataset in files :
 
       treename = output_tree
       tmpinfile = tmpoutfile
-      if tmpoutfile == "tmp1.root" :
-        tmpoutfile = "tmp2.root"
+      if 'tmp1' in tmpoutfile:
+        tmpoutfile = tmpoutfile.replace('tmp1', 'tmp2')
       else :
-        tmpoutfile = "tmp1.root"
+        tmpoutfile = tmpoutfile.replace('tmp2', 'tmp1')
 
-      print command
+      print(command)
       os.system(command)
 
-  os.system("rm %s" % tmpoutfile)
+  if "root://" in output_file:
+    print("xrdcp %s %s" % (tmpinfile, output_file))
+    os.system("xrdcp %s %s" % (tmpinfile, output_file))
+  else:
+    print("mv %s %s" % (tmpinfile, output_file))
   os.system("mv %s %s" % (tmpinfile, output_file))
